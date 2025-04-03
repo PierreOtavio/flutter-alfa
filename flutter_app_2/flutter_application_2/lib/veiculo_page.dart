f7b7f66b6a1d4532a7bc021ad6c4d03b4db15ba4
PierreOtavio <167585576+PierreOtavio@users.noreply.github.com>
2025-04-03 17:33:03 -0300

    03/04/2025
    qlqr coisa me chamem no zap

--- flutter_app_2/flutter_application_2/lib/veiculo_page.dart
+++ flutter_app_2/flutter_application_2/lib/veiculo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/main.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class VeiculoPage extends StatefulWidget {
   const VeiculoPage({Key? key}) : super(key: key);
@@ -6,11 +8,40 @@ class VeiculoPage extends StatefulWidget {
 }
 
 class _VeiculoPageState extends State<VeiculoPage> {
+  String ticket = '';
+
+  readQRCode() async {
+    try 
+    {
+      String code = await FlutterBarcodeScanner.scanBarcode(
+      'FFFFFFF',
+      'Cancelar',
+      true,
+      ScanMode.QR,
+    );
+
+    setState(() => ticket = code != '-1' ? code : 'Inválido');
+    } catch(e)  {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Veiculos')),
-      body: Center(child: Text('Veiculos')),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (ticket != null) 
+            {
+              Padding(
+                child: ,
+              ),
+            },
+          ],
+        ),
+      ),
     );
   }
 }
